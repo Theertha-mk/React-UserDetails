459c8559ce4117d65a1b3b2ebcdee43318345cc7
theertha <theertha@bititude.com>
2022-04-01 17:16:18 +0530

    users list added,modal added,validation added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from "react";
-
-import AddUser from "./components/Users/AddUser";
-
+import React, { useState } from 'react'
+import './App.css'
+import AddUser from './components/Users/AddUser'
+import UsersList from './components/Users/UsersList'
 
 function App() {
+  const [usersList, setUsersList] = useState([])
+  
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ]
+    })
+  }
+
   return (
     <div>
-      <AddUser/>
+      <h1>USER DETAILS</h1>
+      <AddUser onAddUser={addUserHandler} />
+      <h3>USERS LIST</h3>
+      <UsersList users={usersList} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
